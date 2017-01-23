af983e426a24e9009d3a5388bfe6d7fd3e265392
Dmitriy Babkov <dsbabkov@gmail.com>
2017-01-24 00:29:10 +0300

    Выполнено задание 3б

--- lab3/literals.h
+++ lab3/literals.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <stdexcept>
+#include <string>
+#include <climits>
 
 constexpr int charToBin(char ch){
     if (ch != '0' && ch != '1'){
@@ -12,4 +14,22 @@ constexpr int operator "" _b(const char *str, unsigned long count){
     return count ?
             (charToBin(*str) << (count - 1)) + operator""_b(str + 1, count - 1) :
                 0;
+}
+
+std::string operator"" _toBinStr(unsigned long long value){
+    if (!value){
+        return "0b0";
+    }
+
+    constexpr uint nBits = CHAR_BIT *sizeof(value);
+    static const auto bit = [](unsigned long long value, uint i)->int{return (value >> i) & 1;};
+
+    uint i = nBits - 1;
+    for(; !bit(value, i); --i);
+
+    std::string result("0b");
+    for (; i < nBits; --i){
+        result += std::to_string(bit(value, i));
+    }
+    return result;
 }
